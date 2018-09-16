6f8efa8ec8f5483da1c6cfcfe8648668cd0d328e
Xiatian <xiat@ruc.edu.cn>
2018-09-16 11:35:28 +0800

    完成Board任务类型的处理

--- src/main/scala/xiatian/spider/task/BoardFetchTask.scala
+++ src/main/scala/xiatian/spider/task/BoardFetchTask.scala
@@ -2,6 +2,7 @@ package xiatian.spider.task
 
 import org.slf4j.LoggerFactory
 import xiatian.spider.model._
+import xiatian.spider.tool.UrlTransformer
 
 /**
   * 基于XML任务描述文件，构造Board，形成抓取任务，和常用的ArticleHubFetchTask功能
@@ -20,58 +21,58 @@ case class BoardFetchTask(board: Board) extends FetchTask {
 
   /** 该任务下可以处理的链接类型 */
   override def types: Set[LinkType] = Set(ArticleLink, HubLink)
+
   /**
     * 是否接收该链接，作为本任务的一个抓取链接. 如果可以，返回link，否则返回None
     *
     * @param link
     */
-  override def filter(link: FetchLink): Option[FetchLink] = link.`type` match {
-    case ArticleLink =>
-      if (link.depth > maxDepth + 1) None
-      else {
-        if (board.articleFilter.urlRule.r.pattern.matcher(link.url).matches()
-          && !board.articleFilter.notUrlRule.r.pattern.matcher(link.url).matches()
+  override def filter(link: FetchLink): Option[FetchLink] = {
+    val url = link.url
+
+    link.`type` match {
+      case ArticleLink =>
+        //最大深度的导航页里面采集到的文章链接，也可以抓取，所以有效深度要比导航页多1.
+        if (link.depth > board.gatherDepth + 1)
+          None
+        else if (board.articleFilter.urlRule.r.pattern.matcher(url).matches()
+          && !board.articleFilter.notUrlRule.r.pattern.matcher(url).matches()
           && board.articleFilter.minAnchorLength <= link.anchor.length) {
-          //文章链接
-          if (LOG.isDebugEnabled) {
-            LOG.debug(s"Article link: $anchor ==> $url")
-          }
 
           //如果是可跳转的链接，获取其跳转后的链接
           val transformed = UrlTransformer.transform(url)
           val articleUrl = if (transformed.isEmpty) url else transformed.get
 
           if (articleUrl.isEmpty) {
-            log.warn(s"can not extract followed url from $url")
+            LOG.warn(s"can not extract followed url from $url")
             None
           } else {
             if (articleUrl != url) {
-              log.info(s"extracted article $articleUrl from $url")
+              LOG.info(s"extracted article $articleUrl from $url")
             }
             Some(FetchLink(
               articleUrl,
               Some(link.url),
-              anchor,
-              link.depth + 1,
-              FetchLink.ARTICLE_DATA_TYPE,
-              link.boardId,
-              link.params + ("loc" -> ((idx + 1) * 100 / totalPairs).toString)
+              link.anchor,
+              link.depth,
+              link.retries,
+              link.`type`,
+              link.taskId,
+              link.params
             )) //把链接在页面中的位置，转换为0，100之间的数字
           }
-        } else if (context.nextPageUrlRule.r.pattern.matcher(url).matches()) {
-          Some(FetchLink(url, Some(link.url), anchor, link.depth + 1, FetchLink.ARTICLE_NAV_TYPE,
-            link.boardId, link.params))
+        } else None
+      case HubLink =>
+        if (link.depth > board.gatherDepth)
+          None
+        else if (board.nextPageUrlRule.r.pattern.matcher(url).matches()) {
+          Some(link)
         } else {
-          log.debug(s"SKIP link: $anchor ==> $url")
-
+          LOG.debug(s"SKIP nav link: ${link.anchor} ==> $url")
           None
         }
-      }
-
-        Some(link)
-    case HubLink =>
-      if (link.depth > maxDepth) None else Some(link)
-    case _ => None
+      case _ => None
+    }
   }
 
   /**
@@ -81,10 +82,11 @@ case class BoardFetchTask(board: Board) extends FetchTask {
     * @return
     */
   override def nextFetchSeconds(link: FetchLink): Option[Long] = link.`type` match {
-    case ArticleLink => None //文章链接永不重复抓取
+    case ArticleLink =>
+      Option.empty[Long] //文章链接永不重复抓取
     case HubLink =>
       val depth = if (link.depth > 1) link.depth else 1
-      val seconds = refreshInterval * (Math.pow(2, link.retries)).toLong * depth
+      val seconds = board.reqTimeInterval * (Math.pow(2, link.retries)).toLong * depth
       Some(seconds)
   }
 }
