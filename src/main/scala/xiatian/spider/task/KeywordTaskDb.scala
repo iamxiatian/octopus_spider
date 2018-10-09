ebe0ba3552a5c6c853849d98158c346fd3784389
Xiatian <xiat@ruc.edu.cn>
2018-10-09 23:19:14 +0800

    开始设计关键词监控的处理

--- src/main/scala/xiatian/spider/task/KeywordTaskDb.scala
+++ src/main/scala/xiatian/spider/task/KeywordTaskDb.scala
@@ -8,7 +8,12 @@ object KeywordTaskDb {
 
 }
 
-case class Keyword(word: String)
+/**
+  * 监控的关键词，以及该关键词所需要监控的搜索来源
+  * @param word 监控的关键词
+  * @param engines 关键词检索引擎的(名称,更新频率)二元组
+  */
+case class Keyword(word: String, engines: List[(String, Int)])
 
 /**
   * 关键词搜索的引擎，例如百度新闻、知乎、搜索等
