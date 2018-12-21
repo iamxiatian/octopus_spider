d2120a00be83d030fada791878ee156b8fcc1467
Xiatian <xiat@ruc.edu.cn>
2018-12-21 16:07:38 +0800

    add LOG in db

--- src/main/scala/xiatian/octopus/storage/Db.scala
+++ src/main/scala/xiatian/octopus/storage/Db.scala
@@ -1,11 +1,15 @@
 package xiatian.octopus.storage
 
+import org.slf4j.LoggerFactory
+
 import scala.util.Try
 
 /**
   * 数据库特质
   */
 trait Db {
+  val LOG = LoggerFactory.getLogger(this.getClass)
+
   def open()
 
   def close()
